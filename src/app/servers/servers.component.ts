82fff101cd5898b71c81da65e6461769d0675c9d
Greg <wilusz.gregory@gmail.com>
2018-03-12 18:15:17 +0100

    Added built-in directives

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -15,6 +15,8 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'No server was created!';
   serverName = 'InitialName';
   userName = '';
+  serverCreated = false;
+  servers = ['testserver', 'testserver2'];
 
   constructor() {
     setTimeout(() => {
@@ -26,6 +28,8 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
