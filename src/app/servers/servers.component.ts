e8dec21243a8576b0d4eb2fb9e83835be92cbf12
Greg <wilusz.gregory@gmail.com>
2018-03-12 19:21:26 +0100

    Added solution to Assignment 3

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,15 +8,23 @@ import { Component, OnInit } from '@angular/core';
   //     `<app-server></app-server>
   //     <app-server></app-server>`,
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  // styleUrls: ['./servers.component.css']
+  styles: [`
+    .laterClicks {
+      background-color: blue;
+      color: white
+    }
+  `]
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false; // we added a new property holding a boolean value
   serverCreationStatus = 'No server was created!';
   serverName = 'InitialName';
-  userName = '';
+  userName = ''; // used for the purpose of Assignment 2
+  displayContent = false;
   serverCreated = false;
   servers = ['testserver', 'testserver2'];
+  logs = [];
 
   constructor() {
     setTimeout(() => {
@@ -36,6 +44,12 @@ export class ServersComponent implements OnInit {
   onUpdateServerName(event: Event) {
     console.log(event);
     this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  onToggle() {
+    this.displayContent = !this.displayContent;
+    this.logs.push(new Date());
+    console.log(this.logs);
   }
 
 }
